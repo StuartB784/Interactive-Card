0ab7cb6f2b204638db1a53ab8c14fd603321013f
temp <temp>
2017-09-27 13:29:02 -0400

    Add project files.

---
+++ Interactive Card/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Interactive_Card
+{
+    partial class InteractiveCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InteractiveCard));
+            this.ScutumTitle = new System.Windows.Forms.Label();
+            this.Author = new System.Windows.Forms.Label();
+            this.Scutum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ScutumTitle
+            // 
+            this.ScutumTitle.AutoSize = true;
+            this.ScutumTitle.BackColor = System.Drawing.Color.Transparent;
+            this.ScutumTitle.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScutumTitle.ForeColor = System.Drawing.Color.Teal;
+            this.ScutumTitle.Location = new System.Drawing.Point(21, 24);
+            this.ScutumTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ScutumTitle.Name = "ScutumTitle";
+            this.ScutumTitle.Size = new System.Drawing.Size(266, 86);
+            this.ScutumTitle.TabIndex = 0;
+            this.ScutumTitle.Text = "Scutum";
+            // 
+            // Author
+            // 
+            this.Author.AutoSize = true;
+            this.Author.BackColor = System.Drawing.Color.Transparent;
+            this.Author.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Author.Location = new System.Drawing.Point(14, 295);
+            this.Author.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Author.Name = "Author";
+            this.Author.Size = new System.Drawing.Size(293, 47);
+            this.Author.TabIndex = 1;
+            this.Author.Text = "Stuart Bachellier";
+            // 
+            // Scutum
+            // 
+            this.Scutum.BackColor = System.Drawing.Color.Transparent;
+            this.Scutum.Font = new System.Drawing.Font("Hobo Std", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scutum.Image = global::Interactive_Card.Properties.Resources.Scutum;
+            this.Scutum.Location = new System.Drawing.Point(100, 110);
+            this.Scutum.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Scutum.Name = "Scutum";
+            this.Scutum.Size = new System.Drawing.Size(105, 140);
+            this.Scutum.TabIndex = 2;
+            this.Scutum.UseWaitCursor = true;
+            // 
+            // InteractiveCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(4F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::Interactive_Card.Properties.Resources.Space;
+            this.ClientSize = new System.Drawing.Size(314, 351);
+            this.Controls.Add(this.Scutum);
+            this.Controls.Add(this.Author);
+            this.Controls.Add(this.ScutumTitle);
+            this.Font = new System.Drawing.Font("Mistral", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "InteractiveCard";
+            this.Text = "Interactive Card";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ScutumTitle;
+        private System.Windows.Forms.Label Author;
+        private System.Windows.Forms.Label Scutum;
+    }
+}
+
---
+++ Interactive Card/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace Interactive_Card
+{
+    public partial class InteractiveCard : Form
+    {
+        public InteractiveCard()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics(); //Setting Pen and brush
+            Pen drawPen = new Pen(Color.White, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            BackgroundImage = null; //Clear screen
+            Refresh();
+            ScutumTitle.Visible = false;
+            Refresh();
+            Scutum.Visible = false;
+            Refresh();
+
+            formGraphics.FillEllipse(drawBrush, 25, 25, 10, 10); //Draw Constellation
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Ding);
+            player.Play();
+            Thread.Sleep(1000);
+            formGraphics.FillEllipse(drawBrush, 45, 115, 10, 10);
+            player.Play();
+            Thread.Sleep(1000);
+            formGraphics.FillEllipse(drawBrush, 100, 115, 10, 10);
+            player.Play();
+            Thread.Sleep(1000);
+            formGraphics.FillEllipse(drawBrush, 50, 135, 10, 10);
+            player.Play();
+            Thread.Sleep(1000);
+            formGraphics.FillEllipse(drawBrush, 120, 265, 10, 10);
+            player.Play();
+            Thread.Sleep(1000);
+
+            formGraphics.FillEllipse(drawBrush, 250, 152, 2, 2); //Random Stars
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 60, 143, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 345, 380, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 95, 200, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 23, 234, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 274, 97, 2, 2); 
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 21, 346, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 321, 380, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 290, 200, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 170, 234, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 250, 50, 2, 2); 
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 60, 190, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 345, 330, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 95, 295, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 23, 140, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 120, 50, 2, 2); 
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 143, 190, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 100, 110, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 95, 130, 2, 2);
+            Thread.Sleep(100);
+            formGraphics.FillEllipse(drawBrush, 60, 130, 2, 2);
+            Thread.Sleep(100);
+
+            Pen drawPen1 = new Pen(Color.Teal, 3); //Change pen color
+            drawBrush.Color = Color.Teal;
+
+            formGraphics.FillEllipse(drawBrush, 25, 25, 10, 10); //Re-Draw Constellation
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 45, 115, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 100, 115, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 50, 135, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 120, 265, 10, 10);
+            Thread.Sleep(350);
+
+            formGraphics.DrawLine(drawPen1, 29, 29, 48, 118); //Connect Constellation
+            formGraphics.DrawLine(drawPen1, 29, 29, 103, 118);
+            formGraphics.DrawLine(drawPen1, 104, 119, 123, 268);
+            formGraphics.DrawLine(drawPen1, 54, 139, 48, 118);
+            formGraphics.DrawLine(drawPen1, 124, 269, 53, 138);
+
+            Pen drawPen2 = new Pen(Color.BurlyWood, 3); //Change Constellation Color
+            drawBrush.Color = Color.BurlyWood;
+
+            formGraphics.FillEllipse(drawBrush, 25, 25, 10, 10); //Redraw with new color
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 45, 115, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 100, 115, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 50, 135, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 120, 265, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.DrawLine(drawPen2, 29, 29, 48, 118); 
+            formGraphics.DrawLine(drawPen2, 29, 29, 103, 118);
+            formGraphics.DrawLine(drawPen2, 104, 119, 123, 268);
+            formGraphics.DrawLine(drawPen2, 54, 139, 48, 118);
+            formGraphics.DrawLine(drawPen2, 124, 269, 53, 138);
+
+            Pen drawPen3 = new Pen(Color.Fuchsia, 3); //Change Constellation Color
+            drawBrush.Color = Color.Fuchsia;
+
+            formGraphics.FillEllipse(drawBrush, 25, 25, 10, 10); //Redraw with new color
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 45, 115, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 100, 115, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 50, 135, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.FillEllipse(drawBrush, 120, 265, 10, 10);
+            Thread.Sleep(350);
+            formGraphics.DrawLine(drawPen3, 29, 29, 48, 118);
+            formGraphics.DrawLine(drawPen3, 29, 29, 103, 118);
+            formGraphics.DrawLine(drawPen3, 104, 119, 123, 268);
+            formGraphics.DrawLine(drawPen3, 54, 139, 48, 118);
+            formGraphics.DrawLine(drawPen3, 124, 269, 53, 138);
+
+            Graphics fg = this.CreateGraphics(); //Create Text about the sheild
+            Font drawFont = new Font("Mistral", 24, FontStyle.Bold);
+            SolidBrush drawBrush1 = new SolidBrush(Color.Maroon);
+            fg.DrawString("Scutum", drawFont, drawBrush1, 200, 230);
+            Font drawFont1 = new Font("Hobo STD", 12, FontStyle.Bold);
+            fg.DrawString("The Roman Sheild.", drawFont1, drawBrush1, 150, 260);
+            SoundPlayer player1 = new SoundPlayer(Properties.Resources.Cheer);
+            player1.Play();
+        }
+    }
+}
